7d92d3156db80f45a7112033057e067a06e0425f
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-06 01:24:28 +0200

    Users fetching logic works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { useState } from "react";
+import UserInfo from "./components/UserInfo";
 import MainLayout from "./components/MainLayout";
 import {
   createBrowserRouter,
@@ -8,10 +10,16 @@ import {
 } from "react-router-dom";
 
 const App = () => {
+  const [users, setUsers] = useState([]); //State to hold the fetched users
+
+  //Function to handle the data fetched by UserInfo
+  const handleUserFetch = (data) => {
+    setUsers(data);
+  };
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-        <Route index element={""} />
+        <Route index element={<UserInfo onUserFetch={handleUserFetch} />} />
       </Route>
     )
   );
