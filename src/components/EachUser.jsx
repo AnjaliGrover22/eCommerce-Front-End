4a84c7d099f25e255617859eb3119b52c23c006a
AnjaliGrover22 <anjaligrover2210@gmail.com>
2024-09-06 12:40:12 +0200

    designed user page

--- src/components/EachUser.jsx
+++ src/components/EachUser.jsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
+
 const EachUser = ({ user }) => {
   return (
-    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}>
-      {user ? (
-        <>
-          <h3>{user.name}</h3>
-          <h3>{user.email}</h3>
-          <h3>{user.address}</h3>
-          <button>Orders</button>
-        </>
-      ) : (
-        <p>No users fetched yet</p>
-      )}
-    </div>
+    <tr className="odd:bg-gray-100 even:bg-white">
+      <td className="px-6 py-4 text-sm text-gray-900 capitalize">
+        {user.name}
+      </td>
+      <td className="px-6 py-4 text-sm text-gray-900">{user.address}</td>
+      <td className="px-6 py-4 text-sm text-gray-900">{user.email}</td>
+      <td className="px-6 py-4 text-sm text-gray-900">
+        <Link
+          to={""} // Assuming you want to navigate to a detailed page for the user
+          className="inline-block px-4 py-2 text-white bg-custom-blue rounded hover:bg-blue-600"
+        >
+          Details
+        </Link>
+      </td>
+    </tr>
   );
 };
 
